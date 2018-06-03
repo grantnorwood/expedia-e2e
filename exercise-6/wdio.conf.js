22ebaa1d3532d0a3006fdb8206076136e608f752
Grant Norwood <gnorwood@homeaway.com>
2018-06-03 02:53:13 -0500

    Disable running solution code.

--- exercise-6/wdio.conf.js
+++ exercise-6/wdio.conf.js
@@ -10,12 +10,12 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        // './tests/browser/**/*.spec.js'
-        './tests/browser/solution/**/*.spec.js'
+        './tests/browser/**/*.spec.js'
+        // './tests/browser/solution/**/*.spec.js'
     ],
     // Patterns to exclude.
     exclude: [
-        // './tests/browser/solution/**/*.spec.js'
+        './tests/browser/solution/**/*.spec.js'
     ],
     //
     // ============
