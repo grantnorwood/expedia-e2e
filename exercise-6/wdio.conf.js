bc3b33d0dc3e4f8b68beb1869e68266965c8f636
Grant Norwood <gnorwood@homeaway.com>
2018-11-10 20:25:36 -0600

    Updated exercise-6 and 7 to use the spec reporter.

--- exercise-6/wdio.conf.js
+++ exercise-6/wdio.conf.js
@@ -128,7 +128,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: http://webdriver.io/guide/reporters/dot.html
-    reporters: ['dot'],
+    reporters: ['spec'],
     
     //
     // Options to be passed to Mocha.
